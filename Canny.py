d9293d52c09d2e94cec9d6b1932ea5378f5069a1
root <root@JoseToni.localdomain>
2025-09-07 19:38:24 -0600

    Añadi comentarios al codigo

--- Canny.py
+++ Canny.py
@@ -1,10 +1,13 @@
+#Biblioteca opencv
 import cv2
 
+#Abrir imagen
 img = cv2.imread('cartas.jpg',0)
+#Aplicar Canny
 bordeCanny = cv2.Canny(img,100,200)
-
+#Mostrar imagenes
 cv2.imshow('Original', img)
 cv2.imshow('blur', bordeCanny)
-
+#Presionar una tecla oara dejar de ejecutar
 cv2.waitKey(0)
 cv2.destroyAllWindows()
